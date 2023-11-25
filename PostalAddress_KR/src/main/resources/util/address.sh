c63fb0072261bd56b6d5b1f6884be77e0910e808
jkim70 <jihwan11@gmail.com>
2023-11-25 19:10:31 +0900

    Add a shell script file and update the README

---
+++ PostalAddress_KR/src/main/resources/util/address.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+FILES=`ls $1`
+for TOKEN in $FILES
+do
+   LINE=`sed -n '2p;3q' $TOKEN`
+   DO_NAME=`echo $LINE | cut -d\| -f3`
+   sed 's/,/ /g' $TOKEN | sed 's/|/,/g' > $TOKEN.cpy
+   cp $TOKEN.cpy $DO_NAME.csv
+done
+
+rm *.txt *.cpy
